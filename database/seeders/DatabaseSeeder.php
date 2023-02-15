a2b99b3102067f6ec23c8c8993ad27d9a569d208
vladimirbalin <vladimirzq@Gmail.com>
2023-02-15 11:30:27 +0700

    init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Diplom;
+use App\Models\Lector;
+use App\Models\Lecture;
+use App\Models\User;
+use Database\Factories\DiplomFactory;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
