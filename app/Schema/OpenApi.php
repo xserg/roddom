19599a4c5bc7eab3dca431d3a691c4d3fbbebf2f
vladimirbalin <vladimirzq@Gmail.com>
2023-02-17 18:18:29 +0700

    Change name of 'mother' property mother users table, now its 'is_mother'
    Add documentation to login and register endpoints, add ValidationErrors and UserResource schemas

--- app/Schema/OpenApi.php
+++ app/Schema/OpenApi.php
@@ -6,54 +6,23 @@ use OpenApi\Attributes as OA;
 
 #[OA\Info(
     version: "1.0.0",
-    title: "Mothers school API"
+    title: "Mothers school API",
 )]
 #[OA\Contact(
     email: "vladimir.balin@bboom.pro"
 )]
 #[OA\Server(
     url: "https://мамы.online/api/v1/",
+    description: "API Server",
 )]
 #[OA\SecurityScheme(
-    securityScheme: "api key",
-    type: "apiKey",
+    securityScheme: "bearerAuth",
+    type: "http",
     description: "Authorization: Bearer {#api key}",
-    name: "Bearer token",
-    in: "header"
+    name: "bearerAuth",
+    in: "header",
+    scheme: "bearer",
 )]
-///**
-// * @OA\SecurityScheme(
-// *      securityScheme="xsrf",
-// *      type="apiKey",
-// *      in="cookie",
-// *      name="XSRF-TOKEN"
-// * ),
-// * @OA\SecurityScheme(
-// *      securityScheme="session-id",
-// *      type="apiKey",
-// *      in="cookie",
-// *      name="notes_laravel_session"
-// * ),
-// * @OA\Schema(
-// *      schema="note-resource",
-// *      title="Note resource",
-// *      type="object",
-// *      @OA\Property(property="id", type="integer"),
-// *      @OA\Property(property="title", type="string"),
-// *      @OA\Property(property="content", type="string"),
-// *      @OA\Property(property="created_at", type="string", format="date-time"),
-// * )
-// * @OA\Schema(
-// *      title="User model",
-// *      schema="user",
-// *      @OA\Property(property="id", type="integer",),
-// *      @OA\Property(property="name", type="string",),
-// *      @OA\Property(property="email", type="string", format="email"),
-// *      @OA\Property(property="email_verified_at", type="string", format="date-time",),
-// *      @OA\Property(property="created_at", type="string", format="date-time",),
-// *      @OA\Property(property="updated_at", type="string", format="date-time",),
-// * )
-// */
 class OpenApi
 {
 }
