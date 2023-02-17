2c5c6e5908ed6a3eaae69194ded944ea15f1f862
vladimirbalin <vladimirzq@Gmail.com>
2023-02-17 11:37:50 +0700

    Add docs folder, start to documenting api

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -6,7 +6,36 @@ use App\Http\Requests\RegisterRequest;
 use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use OpenApi\Attributes as OA;
 
+#[OA\Get(path: '/register')]
+#[OA\RequestBody (
+    description: "Login credentials",
+    required: true,
+    content: [new OA\JsonContent(
+        properties: [
+            new OA\Property(
+                property: "email",
+                description: "Email пользователя",
+                type: "string"
+            ),
+            new OA\Property(
+                property: "password",
+                description: "Пароль пользователя",
+                type: "string",
+                maxLength: 255,
+                minLength: 6
+            ),
+            new OA\Property(
+                property: "password_confirmation",
+                description: "Подтверждение пароля",
+                type: "string"
+            ),
+        ]
+    )]
+)]
+#[OA\Response(response: 200, description: 'AOK')]
+#[OA\Response(response: 401, description: 'Not allowed')]
 class RegisterController
 {
     public function __construct(
