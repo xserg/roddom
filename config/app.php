a8f194f0771f073dcd2f488630f4a7129203ff11
vladimirbalin <vladimirzq@Gmail.com>
2023-03-20 16:05:54 +0700

    Add back to site url, after payment

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,6 +55,8 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+
+    'back2site' => env('BACK_TO_SITE_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', '/'),
 
