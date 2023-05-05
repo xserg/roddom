9af207e0ede4223fba24db863cdbc6efb8fda392
vladimirbalin <vladimirzq@Gmail.com>
2023-05-05 21:37:12 +0700

    Add migration with string now longer than 256

---
+++ database/migrations/2023_05_03_101950_add_columns_to_lectures_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lectures', function (Blueprint $table) {
+            $table->string('content', 10240)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lectures', function (Blueprint $table) {
+        });
+    }
+};
