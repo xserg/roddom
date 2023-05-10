9d4aaa18d076e56fc4420de2b9d69540a27df7d2
vladimirbalin <vladimirzq@Gmail.com>
2023-05-10 13:33:03 +0700

    Change RegisterController.php now it creates new user, new login code, send login code

--- app/Http/Controllers/Api/User/RegisterController.php
+++ app/Http/Controllers/Api/User/RegisterController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Http\Requests\RegisterRequest;
-use App\Http\Resources\UserResource;
+use App\Mail\SendLoginCode;
+use App\Services\LoginCodeService;
 use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Mail;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -30,11 +32,11 @@ use Symfony\Component\HttpFoundation\Response;
     content: [new OA\MediaType(mediaType: 'application/json', schema: new OA\Schema(ref: '#/components/schemas/ValidationErrors'))],
 )]
 #[OA\Response(response: 500, description: 'Server Error')]
-
 class RegisterController
 {
     public function __construct(
-        private UserService $userService
+        private UserService      $userService,
+        private LoginCodeService $loginCodeService
     )
     {
     }
@@ -46,19 +48,26 @@ class RegisterController
 
         try {
             $user = $this->userService->create(compact('email', 'password'));
+            $code = mt_rand(100000, 999999);
+            $this->loginCodeService->create($email, $code);
+
         } catch (\Exception $exception) {
             return response()->json(
                 ['message' => $exception->getMessage()],
                 Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+        $sent = Mail::to($email)
+            ->send(new SendLoginCode($code));
 
-        return response()->json(
-            [
-                'user' => new UserResource($user),
-                'access_token' => $token,
-                'token_type' => 'Bearer'
-            ], Response::HTTP_CREATED);
+        if (!$sent) {
+            return response()->json([
+                'message' => 'Невозможно послать email с кодом логина'
+            ], \Illuminate\Http\Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'message' => 'Код отослан на ваш email',
+        ], Response::HTTP_OK);
     }
 }
