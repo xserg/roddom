bb2b7ad23fd6c76fdc4a1b4eb29d368a55943ea8
vladimirbalin <vladimirzq@Gmail.com>
2023-05-27 14:19:17 +0700

    Add belongsTo method in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
@@ -18,4 +19,14 @@ class Order extends Model
         'subscriptionable_id',
         'period',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function userEmail(): string
+    {
+        return $this->user->email;
+    }
 }
