645bad50e9e8ae7b7a8a76a6bf6473653545b399
vladimirB <vladimirzq@gmail.com>
2023-07-13 14:42:45 +0700

    Add sort to SubscriptionResource.php

--- app/Filament/Resources/SubscriptionResource.php
+++ app/Filament/Resources/SubscriptionResource.php
@@ -155,7 +155,7 @@ class SubscriptionResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-//            ->defaultSort('created_at', 'desc')
+            ->defaultSort('created_at', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('user.name')
                     ->label('Пользователь')
