f943923558209c7c13c0116a2daebbb547305f31
vladimirB <vladimirzq@gmail.com>
2023-07-16 18:09:35 +0700

    Change WizardController.php return, add common titles

--- app/Http/Controllers/Api/Wizard/WizardController.php
+++ app/Http/Controllers/Api/Wizard/WizardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Wizard;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\WizardResource;
 use App\Models\Wizard;
+use App\Models\WizardInfo;
 use Illuminate\Http\Request;
 
 class WizardController extends Controller
@@ -15,6 +16,9 @@ class WizardController extends Controller
 
     public function __invoke(Request $request)
     {
-        return WizardResource::collection(Wizard::query()->orderBy('order')->get());
+        return response()->json([
+            'common_titles' => WizardInfo::query()->pluck( 'value', 'key'),
+            'data' => WizardResource::collection(Wizard::query()->orderBy('order')->get()),
+        ]);
     }
 }
