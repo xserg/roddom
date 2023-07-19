474110886e00c8af33914815f83cb9ad950b7204
vladimirB <vladimirzq@gmail.com>
2023-07-19 17:21:16 +0700

    Change console queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('queue:work')
+        $schedule->command('queue:work --queue=' . config('queue.connections.database.queue'))
             ->everyMinute()
             ->withoutOverlapping();
 
