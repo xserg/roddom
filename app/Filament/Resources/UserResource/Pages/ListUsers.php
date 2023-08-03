620814dd0dada110ba4ad22d7c219afe0fb55be6
vladimirB <vladimirzq@gmail.com>
2023-08-03 22:38:09 +0700

    Add UserResource babycoins aggregations

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Contracts\View\View;
@@ -11,11 +12,20 @@ class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
     protected static ?string $navigationLabel = 'Пользователи';
-    public array $data_list= [
+    public array $data_list = [
         'calc_columns' => [
             'refPoints.points',
         ],
+        'common_babycoins' => 0
     ];
+
+    public function __construct($id = null)
+    {
+        parent::__construct($id);
+        $this->data_list['common_babycoins'] = User::where('is_admin', '!=', 1)
+            ->with('refPoints')->get()
+            ->sum(fn ($user) => $user->refPoints?->points);
+    }
 
     protected function getActions(): array
     {
--- resources/views/table/users-footer.blade.php
+++ resources/views/table/users-footer.blade.php
@@ -11,11 +11,14 @@
             @for ($i = 0; $i < count($calc_columns); $i++ )
                 @if ($column->getName() == $calc_columns[$i])
                     <div class="filament-tables-column-wrapper">
-                        <div class="filament-tables-text-column px-4 py-2 flex w-full justify-start text-start">
-                            <div class="inline-flex items-center space-x-1 rtl:space-x-reverse">
-                                <span class="font-medium">
-                                    {{ $records->sum($calc_columns[$i]) / 100 }}
-                                </span>
+                        <div class="filament-tables-text-column px-4 py-2 flex flex-col w-full justify-start text-start">
+                            <div class="items-center space-x-1 rtl:space-x-reverse">
+                                <div class="font-medium">
+                                    <span class="font-light">Страница: </span> {{ $records->sum($calc_columns[$i]) / 100 }}
+                                </div>
+                                <div class="font-medium">
+                                    <span class="font-light">Все: </span>{{ $common_babycoins / 100 }}
+                                </div>
                             </div>
                         </div>
                     </div>
