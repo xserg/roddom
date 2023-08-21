948aa70bee97310c860bfad9a47d51e18c24e2e1
vladimirB <vladimirzq@gmail.com>
2023-08-21 14:16:30 +0700

    Throttling

--- app/Http/Cache/Limit.php
+++ app/Http/Cache/Limit.php
@@ -5,6 +5,20 @@ namespace App\Http\Cache;
 class Limit extends \Illuminate\Cache\RateLimiting\Limit
 {
     /**
+     * Create a new limit instance.
+     *
+     * @param  mixed|string  $key
+     * @param  int  $maxAttempts
+     * @param  int|float  $decayMinutes
+     * @return void
+     */
+    public function __construct($key = '', int $maxAttempts = 60, $decayMinutes = 1)
+    {
+        $this->key = $key;
+        $this->maxAttempts = $maxAttempts;
+        $this->decayMinutes = $decayMinutes;
+    }
+    /**
      * Create a new rate limit using seconds as decay time.
      *
      * @param  int  $decaySeconds
