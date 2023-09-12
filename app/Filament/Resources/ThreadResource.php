2612328349058cdaaa7e5cac2a15154d83a0129c
vladimirB <vladimirzq@gmail.com>
2023-09-12 21:15:34 +0700

    Badge fix

--- app/Filament/Resources/ThreadResource.php
+++ app/Filament/Resources/ThreadResource.php
@@ -145,7 +145,10 @@ class ThreadResource extends Resource
         //compare users read_at and thread read_at, count it
         $count = 0;
         $threads->each(function (Thread $thread) use (&$count) {
-            if ($thread->updated_at > $thread->participantForUser(auth()->id())?->read_at) {
+            if (
+                $thread->participantForUser(auth()->id()) &&
+                $thread->updated_at > $thread->participantForUser(auth()->id())?->read_at
+            ) {
                 $count++;
             }
         });
