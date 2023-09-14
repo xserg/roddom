33ea3909100bc5af568e1e9806bf21d400805749
vladimirB <vladimirzq@gmail.com>
2023-09-15 00:01:21 +0700

    Refactoring PaymentController.php

--- app/Http/Controllers/Api/Payment/PaymentController.php
+++ app/Http/Controllers/Api/Payment/PaymentController.php
@@ -4,26 +4,10 @@ namespace App\Http\Controllers\Api\Payment;
 
 use App\Enums\PaymentStatusEnum;
 use App\Http\Controllers\Controller;
-use App\Jobs\SyncSubscriptionItemsJob;
-use App\Mail\PurchaseSuccess;
-use App\Models\AppInfo;
-use App\Models\Category;
-use App\Models\Lecture;
 use App\Models\Order;
-use App\Models\Period;
-use App\Models\RefInfo;
-use App\Models\RefPoints;
-use App\Models\RefPointsPayments;
-use App\Models\Subscription;
-use App\Models\User;
 use App\Repositories\PeriodRepository;
 use App\Services\PaymentService;
-use App\Services\UserService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Storage;
 use YooKassa\Common\Exceptions\ApiException;
 use YooKassa\Common\Exceptions\BadApiRequestException;
 use YooKassa\Common\Exceptions\ExtensionNotFoundException;
@@ -43,7 +27,6 @@ class PaymentController extends Controller
     public function __construct(
         private PaymentService   $paymentService,
         private PeriodRepository $periodRepository,
-        private UserService      $userService,
     ) {
     }
 
@@ -89,84 +72,26 @@ class PaymentController extends Controller
             ], $payment->id, uniqid('', true));
         }
 
-        if (isset($payment->status) && $payment->status === 'succeeded') {
-            if ($payment->paid === true) {
-                $metadata = $payment->metadata;
+        if (
+            (isset($payment->status) && $payment->status === 'succeeded')
+            && $payment->paid === true
+        ) {
+            $metadata = $payment->metadata;
 
-                if (isset($metadata->order_id)) {
-                    $orderId = (int) $metadata->order_id;
-                    $order = Order::query()->findOrFail($orderId);
-
-                    if ($order->isConfirmed()) {
-                        return;
-                    }
-
-                    $period = $this->periodRepository->getPeriodByLength($order->period);
-                    $subscriptionAttributes = $this->getSubscriptionAttributes(
-                        $order, $period
-                    );
-
-                    $subscription = new Subscription($subscriptionAttributes);
-
-                    DB::transaction(function () use (
-                        $order,
-                        $subscription,
-                    ) {
-                        $order->status = PaymentStatusEnum::CONFIRMED;
-                        /**
-                         * @var User $orderedUser
-                         */
-                        $orderedUser = $order->user;
-                        if ($order->points) {
-                            $orderedUser->refPoints()->decrement('points', $order->points);
-                            $orderedUser->refPointsMadePayments()->create([
-                                'ref_points' => $order->points,
-                                'reason' => RefPointsPayments::REASON_BUY,
-                                'price' => $order->price,
-                            ]);
-                        }
-                        $order->save();
-                        $subscription->save();
-                    });
-
-                    $this->userService->rewardReferrersForBuying($order, $order->user);
-                    $appInfo = AppInfo::first();
-                    $successfulPurchaseText = $appInfo?->successful_purchase_text ?? 'Спасибо за покупку';
-                    $email = $order->userEmail();
-                    $image = Storage::url($appInfo->successful_purchase_image);
-                    $appLink = $appInfo?->app_link_share_link ?? config('app.frontend_url');
-                    $appName = $appInfo?->app_title ?? config('app.name');
-
-                    Mail::to($email)
-                        ->send(
-                            (new PurchaseSuccess(
-                                'Успешная покупка',
-                                $appLink,
-                                $appName,
-                                $successfulPurchaseText,
-                                $subscription->entity_title,
-                                $subscription->start_date->isoFormat('HH:mm DD.MM.YYYY'),
-                                $subscription->end_date->isoFormat('HH:mm DD.MM.YYYY'),
-                                $image
-                            )));
-                }
+            if (! isset($metadata->order_id)) {
+                return;
             }
-        }
-    }
 
-    private function getSubscriptionAttributes(
-        Order  $order,
-        Period $period,
-    ): array {
-        return [
-            'user_id' => $order->user_id,
-            'subscriptionable_type' => $order->subscriptionable_type,
-            'subscriptionable_id' => $order->subscriptionable_id,
-            'period_id' => $period->id,
-            'total_price' => $order->price,
-            'points' => $order->points,
-            'start_date' => now(),
-            'end_date' => now()->addDays($period->length),
-        ];
+            $orderId = (int) $metadata->order_id;
+            $order = Order::query()->findOrFail($orderId);
+
+            if ($order->isConfirmed()) {
+                return;
+            }
+
+            $period = $this->periodRepository->getPeriodByLength($order->period);
+
+            $this->paymentService->confirmOrder($order, $period);
+        }
     }
 }
--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -2,6 +2,18 @@
 
 namespace App\Services;
 
+use App\Enums\PaymentStatusEnum;
+use App\Mail\PurchaseSuccess;
+use App\Models\AppInfo;
+use App\Models\Order;
+use App\Models\Period;
+use App\Models\RefInfo;
+use App\Models\RefPointsPayments;
+use App\Models\Subscription;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 use YooKassa\Client;
 
 class PaymentService
@@ -33,5 +45,155 @@ class PaymentService
             ], uniqid('', true));
 
         return $client->getConfirmation()->getConfirmationUrl();
+    }
+
+    public function confirmOrder(Order $order, Period $period): void
+    {
+        $subscriptionAttributes = $this->getSubscriptionAttributes(
+            $order, $period
+        );
+
+        $subscription = new Subscription($subscriptionAttributes);
+
+        DB::transaction(function () use (
+            $order,
+            $subscription,
+        ) {
+            $order->status = PaymentStatusEnum::CONFIRMED;
+            /**
+             * @var User $orderedUser
+             */
+            $orderedUser = $order->user;
+            if ($order->points) {
+                $orderedUser->refPoints()->decrement('points', $order->points);
+                $orderedUser->refPointsMadePayments()->create([
+                    'ref_points' => $order->points,
+                    'reason' => RefPointsPayments::REASON_BUY,
+                    'price' => $order->price,
+                ]);
+            }
+            $order->save();
+            $subscription->save();
+        });
+
+        $this->rewardReferrersForBuying($order, $order->user);
+        $appInfo = AppInfo::first();
+        $successfulPurchaseText = $appInfo?->successful_purchase_text ?? 'Спасибо за покупку';
+        $email = $order->userEmail();
+        $image = Storage::url($appInfo->successful_purchase_image);
+        $appLink = $appInfo?->app_link_share_link ?? config('app.frontend_url');
+        $appName = $appInfo?->app_title ?? config('app.name');
+
+        Mail::to($email)
+            ->send((new PurchaseSuccess(
+                'Успешная покупка',
+                $appLink,
+                $appName,
+                $successfulPurchaseText,
+                $subscription->entity_title,
+                $subscription->start_date->isoFormat('HH:mm DD.MM.YYYY'),
+                $subscription->end_date->isoFormat('HH:mm DD.MM.YYYY'),
+                $image
+            )));
+    }
+
+    private function getSubscriptionAttributes(
+        Order  $order,
+        Period $period,
+    ): array {
+        return [
+            'user_id' => $order->user_id,
+            'subscriptionable_type' => $order->subscriptionable_type,
+            'subscriptionable_id' => $order->subscriptionable_id,
+            'period_id' => $period->id,
+            'total_price' => $order->price,
+            'points' => $order->points,
+            'start_date' => now(),
+            'end_date' => now()->addDays($period->length),
+        ];
+    }
+
+    public function rewardReferrersForBuying(Order $order, User $buyer): void
+    {
+        $refInfo = RefInfo::query()->first();
+        $residualAmount = $order->price - $order->points;
+
+        if ($residualAmount > 0) {
+            $relationships = [
+                1 => $buyer->referrer(),
+                2 => $buyer->referrerSecondLevel(),
+                3 => $buyer->referrerThirdLevel(),
+                4 => $buyer->referrerFourthLevel(),
+                5 => $buyer->referrerFifthLevel(),
+            ];
+
+            foreach ($relationships as $depth => $relationship) {
+                if ($relationship->doesntExist()) {
+                    continue;
+                }
+
+                $percent = $refInfo->firstWhere('depth_level', $depth)?->percent ?? 5;
+                $referrer = $relationship->first();
+                $pointsToGet = $residualAmount * ($percent / 100);
+
+                $referrer->refPointsGetPayments()->create([
+                    'payer_id' => $order->user->id,
+                    'reason' => RefPointsPayments::REASON_BUY,
+                    'ref_points' => $pointsToGet,
+                    'price' => $order->price,
+                    'depth_level' => $depth,
+                    'percent' => $percent,
+                ]);
+
+                if ($referrer->refPoints()->exists()) {
+                    $refPoints = $referrer->refPoints;
+                    $refPoints->points += $pointsToGet;
+                    $refPoints->save();
+                } else {
+                    $referrer->refPoints()->create(['points' => $pointsToGet]);
+                }
+            }
+        }
+
+//        adjacency-list
+
+//        if (
+//            $buyer->ancestors()
+//                ->whereDepth('>', -6)
+//                ->whereDepth('<', -1)
+//                ->exists()
+//        ) {
+//            $ancestors = $buyer->ancestors()
+//                ->whereDepth('>', -6)
+//                ->whereDepth('<', -1)
+//                ->get();
+//
+//            $ancestors->each(function ($ancestor) use ($order, $refInfo) {
+//                $percent = $refInfo->firstWhere('depth_level', 2)->percent;
+//                $residualAmount = $order->price - $order->points;
+//
+//                if ($residualAmount <= 0) {
+//                    return;
+//                }
+//
+//                $pointsToGet = $residualAmount * ($percent / 100);
+//                $ancestor->refPointsGetPayments()->create([
+//                    'payer_id' => $order->user->id,
+//                    'reason' => RefPointsPayments::REASON_BUY,
+//                    'ref_points' => $pointsToGet,
+//                    'price' => $order->price,
+//                    'depth_level' => 1,
+//                    'percent' => $percent,
+//                ]);
+//
+//                if ($ancestor->refPoints()->exists()) {
+//                    $refPoints = $ancestor->refPoints;
+//                    $refPoints->points += $pointsToGet;
+//                    $refPoints->save();
+//                } else {
+//                    $ancestor->refPoints()->create(['points' => $pointsToGet]);
+//                }
+//            });
+//        }
     }
 }
