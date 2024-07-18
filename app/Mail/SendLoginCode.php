2828d28e64b6989a64ddfb28c8a509ba5642208d
xserg <xxserg@gmail.com>
2024-07-18 12:57:18 +0300

    login code

--- app/Mail/SendLoginCode.php
+++ app/Mail/SendLoginCode.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendLoginCode extends Mailable implements ShouldQueue
+class SendLoginCode extends Mailable
 {
     use Queueable, SerializesModels;
 
